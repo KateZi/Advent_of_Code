551b9c2c3ad42f12afd6fa0a7b89f56d61772b3f
KateZi <zkate2006@yandex.ru>
2020-02-24 19:13:40 +0200

    Day 1 - draft

---
+++ Day 1.py
@@ -0,0 +1,19 @@
+def calc_fuel(mass):
+    if mass <= 0: return 0
+    else:
+        fuel = mass//3 - 2
+        temp = calc_fuel(fuel)
+        return fuel + temp if temp > 0 else fuel
+
+
+def calc_total():
+    M = 0
+    f = open("Fuel_input", "r")
+    masses = f.readlines()
+    for mass in masses:
+        M += calc_fuel(int(mass))
+
+    print(M)
+
+# print(calc_fuel(100756))
+calc_total()
