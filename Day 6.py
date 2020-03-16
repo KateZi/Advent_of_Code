169414972a9dad65e3bdda61d6ab1f917c7cd994
KateZi <zkate2006@yandex.ru>
2020-03-16 16:31:35 +0200

    Day 6 - Part 1, recursively. It makes more sense for the "moons" to be the keys

--- Day 6.py
+++ Day 6.py
@@ -1,6 +1,6 @@
 orbits = dict()
 
-
+# Part 1
 def count_orbits(planet, sum):
 
     p = orbits.get(planet)
@@ -14,23 +14,31 @@ def count_orbits(planet, sum):
     return sum
 
 
-def main():
-    with open("inputs/Map Data", "r") as f:
-        for line in f:
-            planets = line.split(")")
-            planets[1] = planets[1].rstrip("\n")
-
-            if planets[0] in orbits.keys():
-                orbits[planets[0]].append(planets[1])
-            else:
-                orbits[planets[0]] = [planets[1]]
-
+def run_part1():
     num_orbits = 0
     for key in orbits.keys():
         num_orbits += count_orbits(key, 0)
 
     print(num_orbits)
 
+
+# Part 2
+def run_part2():
+
+
+
+def main():
+    with open("inputs/Map Data", "r") as f:
+        for line in f:
+            planets = line.split(")")
+            planets[1] = planets[1].rstrip("\n")
+
+            if planets[1] in orbits.keys():
+                orbits[planets[1]].append(planets[0])
+            else:
+                orbits[planets[1]] = [planets[0]]
+
+    run_part1()
 
 if __name__=='__main__':
     main()
